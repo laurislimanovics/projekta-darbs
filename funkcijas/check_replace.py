fdfc8b84f979e05e41199547ba2db98a58de80c1
Lauris Limanovičs <Lauris.Limanovics@edu.rtu.lv>
2024-01-14 20:46:13 +0200

    Update check_replace.py

--- funkcijas/check_replace.py
+++ funkcijas/check_replace.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def check_if_being_replaced(dataframe, replacement2_column='Aizvieto'):
-    # Pārbauda, vai kolonnā 'Aizvietotājs' ir ierakstīts teksts
+    # Pārbauda, vai kolonnā 'Aizvieto' ir ierakstīts teksts
     is_replaced = dataframe[replacement2_column].apply(lambda x: not pd.isna(x) and x != '')
     
     # Atgriež rezultātu kā DataFrame kolonnu
