2e8121b8980adfddd16ad50c82feb6f1550bb237
ksenijashitikova <144550653+ksenijashitikova@users.noreply.github.com>
2024-01-16 17:19:34 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Funkcijas importēšana no funkciju failiem
 from funkcijas.avg_salary import average_salary
-
+from funkcijas.count_employees import count_employees_per_division
 # Ielādē datus no Excel faila
 df = pd.read_excel('empl.xlsx')
 education_file_path = 'education.xlsx'
@@ -28,7 +28,14 @@ def generate_pie_chart():
         plt.pie(result.values(), labels=result.keys(), autopct=lambda p: f'€{p*sum(result.values())/100:.2f}' if p > 2 else '', textprops={'fontsize': 10})
         plt.title("Nodaļu vidējās algas")
         plt.show()
+    elif report_type == 'Darbinieku skaits nodaļās':
+        result = count_employees_per_division(division_list, df)
 
+        # Ģenerē tortes diagrammu, pamatojoties uz rezultātu ar vidējo algu
+        plt.figure(figsize=(8, 8)) 
+        plt.pie(result.values(), labels=result.keys(), autopct=lambda p: f'{int(p * sum(result.values()) / 100)}', textprops={'fontsize': 10})
+        plt.title("Darbinieku skaits nodaļās")
+        plt.show()
 # Funkcija tabulas ziņojuma ģenerēšanai
 def generate_table_report():
     # Saņem izvēlēto ziņojuma veidu no nolaižamā saraksta
@@ -67,7 +74,40 @@ def generate_table_report():
         # Konfigurējiet režģus
         table_window.grid_rowconfigure(0, weight=1)
         table_window.grid_columnconfigure(0, weight=1)
+    elif report_type == 'Darbinieku skaits nodaļās': 
+        employee_counts = count_employees_per_division(division_list, df)
 
+        # Izveido jaunu logu tabulas rādīšanai
+        table_window = tk.Toplevel(root)
+        table_window.title('Tabulas ziņojums')
+
+        # Izveido Treeview logrīka izveidošanai
+        tree = ttk.Treeview(table_window, columns=("Nodaļa", "Darbinieku skaits"), show="headings")
+        tree.heading("Nodaļa", text="Nodaļa", anchor="center")  
+        tree.heading("Darbinieku skaits", text="Darbinieku skaits", anchor="center")  
+        tree.column("Nodaļa", width=300)  
+        tree.column("Darbinieku skaits", width=150)
+
+        # Izveido vertikālu ritjoslu
+        tree_scrollbar = ttk.Scrollbar(table_window, orient="vertical", command=tree.yview)
+        tree.configure(yscroll=tree_scrollbar.set)
+
+        # Iepakojiet Treeview un ritjoslu
+        tree.grid(row=0, column=0, sticky="nsew")
+        tree_scrollbar.grid(row=0, column=1, sticky="ns")
+
+        # Ievietojiet datus Treeview logrīkā ar centrētu izlīdzinājumu
+        for division, employee_count in employee_counts.items():
+            tree.insert("", "end", values=(division, f"{employee_count}"))
+
+        # Pievieno režģus Treeview
+        style = ttk.Style()
+        style.configure("Treeview", rowheight=25, font=('Helvetica', 10))
+        tree.tag_configure("centered", anchor="center")
+
+        # Konfigurējiet režģus
+        table_window.grid_rowconfigure(0, weight=1)
+        table_window.grid_columnconfigure(0, weight=1)
 def generate_bar_chart():
     # Saņem izvēlēto ziņojuma veidu no nolaižamā saraksta
     report_type = report_type_var.get()
@@ -89,7 +129,24 @@ def generate_bar_chart():
         
         plt.tight_layout()
         plt.show()
+    elif report_type == 'Darbinieku skaits nodaļās': 
+        employee_counts = count_employees_per_division(division_list, df)
 
+        # Pārvērst vārdnīcas atslēgas sarakstu
+        divisions = list(employee_counts.keys())
+
+        # Ģenerē horizontālu joslu diagrammu ar nodaļām un darbinieku skaitu
+        plt.figure(figsize=(8, 6))
+        bars = plt.barh(divisions, employee_counts.values(), color='royalblue')
+        plt.xlabel('Darbinieku skaits')
+        plt.title('Darbinieku skaits nodaļās')
+
+        # Anotē joslas ar faktiskajiem darbinieku skaita vērtībām uz virsotnēm
+        for bar, count in zip(bars, employee_counts.values()):
+            plt.text(count + 5, bar.get_y() + bar.get_height() / 2, f'{count}', ha='center', va='center', fontsize=10)
+
+        plt.tight_layout()
+        plt.show()
 # Izveido galveno Tkinter logu
 root = tk.Tk()
 root.title('Ziņojumu ģenerators')
