e7d7c8624ce25162d55a57284f678c029b81dd20
Lauris Limanovičs <Lauris.Limanovics@edu.rtu.lv>
2024-01-16 17:38:07 +0200

    Update count_by_address.py

--- funkcijas/count_by_address.py
+++ funkcijas/count_by_address.py
@@ -1,3 +1,3 @@
 def count_employees_by_address(dataframe):
-    address_counts = dataframe['Darba vietas adrese'].value_counts()
+    address_counts = dataframe['Darba vietas adrese'].value_counts().to_dict()
     return address_counts
